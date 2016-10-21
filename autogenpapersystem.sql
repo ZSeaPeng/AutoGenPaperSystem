7347b9ac6acd1aae36061e40d4fd845cf4fff798
sgt110 <1067541968@qq.com>
2016-10-21 16:21:35 +0800

    sql文件更新

--- autogenpapersystem.sql
+++ autogenpapersystem.sql
@@ -10,7 +10,7 @@ Target Server Type    : MYSQL
 Target Server Version : 50552
 File Encoding         : 65001
 
-Date: 2016-10-14 13:15:57
+Date: 2016-10-21 16:17:38
 */
 
 SET FOREIGN_KEY_CHECKS=0;
@@ -80,7 +80,7 @@ CREATE TABLE `knowledge` (
   `Subject_ID` int(11) NOT NULL,
   `Superior_ID` int(11) DEFAULT '0',
   PRIMARY KEY (`Knowledge_ID`)
-) ENGINE=InnoDB AUTO_INCREMENT=30 DEFAULT CHARSET=utf8;
+) ENGINE=InnoDB AUTO_INCREMENT=42 DEFAULT CHARSET=utf8;
 
 -- ----------------------------
 -- Records of knowledge
@@ -111,6 +111,18 @@ INSERT INTO `knowledge` VALUES ('26', '导数', '2', '14');
 INSERT INTO `knowledge` VALUES ('27', '极限', '2', '14');
 INSERT INTO `knowledge` VALUES ('28', '解析式', '2', '19');
 INSERT INTO `knowledge` VALUES ('29', '反函数', '2', '19');
+INSERT INTO `knowledge` VALUES ('30', '人类遗传病', '6', '0');
+INSERT INTO `knowledge` VALUES ('31', '人类遗传病的类型', '6', '30');
+INSERT INTO `knowledge` VALUES ('32', '细胞的代谢', '6', '0');
+INSERT INTO `knowledge` VALUES ('33', '细胞的呼吸', '6', '32');
+INSERT INTO `knowledge` VALUES ('34', '近代世界', '9', '0');
+INSERT INTO `knowledge` VALUES ('35', '近代科学技术', '9', '34');
+INSERT INTO `knowledge` VALUES ('36', '现代世界', '9', '0');
+INSERT INTO `knowledge` VALUES ('37', '现代科学技术', '9', '36');
+INSERT INTO `knowledge` VALUES ('38', '化学实验', '5', '0');
+INSERT INTO `knowledge` VALUES ('39', '实验探究', '5', '38');
+INSERT INTO `knowledge` VALUES ('40', '化学反应原理', '5', '0');
+INSERT INTO `knowledge` VALUES ('41', '电化学', '5', '40');
 
 -- ----------------------------
 -- Table structure for questions
@@ -118,7 +130,8 @@ INSERT INTO `knowledge` VALUES ('29', '反函数', '2', '19');
 DROP TABLE IF EXISTS `questions`;
 CREATE TABLE `questions` (
   `Questions_ID` int(11) NOT NULL AUTO_INCREMENT,
-  `Content` varchar(200) COLLATE utf8_bin NOT NULL,
+  `Content` varchar(500) COLLATE utf8_bin NOT NULL,
+  `QuesPic_URL` varchar(500) COLLATE utf8_bin DEFAULT NULL,
   `Subject_ID` int(11) NOT NULL DEFAULT '0',
   `Knowledge_ID1` int(11) DEFAULT '0',
   `Knowledge_ID2` int(11) DEFAULT '0',
@@ -129,36 +142,57 @@ CREATE TABLE `questions` (
   `Charact_ID` int(11) NOT NULL DEFAULT '0',
   `Isdelete` tinyint(1) NOT NULL DEFAULT '0',
   `Answer` varchar(100) COLLATE utf8_bin DEFAULT '略',
+  `AnswerPic_URL` varchar(500) COLLATE utf8_bin DEFAULT NULL,
   PRIMARY KEY (`Questions_ID`)
-) ENGINE=InnoDB AUTO_INCREMENT=28 DEFAULT CHARSET=utf8 COLLATE=utf8_bin;
+) ENGINE=InnoDB AUTO_INCREMENT=50 DEFAULT CHARSET=utf8 COLLATE=utf8_bin;
 
 -- ----------------------------
 -- Records of questions
 -- ----------------------------
-INSERT INTO `questions` VALUES ('1', 'qewretyytiyopi', '1', '1', '3', '4', '0', '1', '5', '3', '0', '略');
-INSERT INTO `questions` VALUES ('2', 'uijsidofoei', '3', '2', '0', '0', '0', '3', '4', '1', '0', '略');
-INSERT INTO `questions` VALUES ('3', 'dajfioef', '4', '6', '0', '0', '0', '8', '3', '3', '0', '略');
-INSERT INTO `questions` VALUES ('4', 'ffbfb', '5', '2', '0', '0', '0', '7', '2', '2', '0', '略');
-INSERT INTO `questions` VALUES ('8', 'dfefdfef', '6', '11', '16', '0', '0', '6', '3', '1', '0', '略');
-INSERT INTO `questions` VALUES ('9', 'ghdjghg', '3', '9', '0', '0', '0', '13', '3', '2', '0', '略');
-INSERT INTO `questions` VALUES ('10', 'trhtjfgt', '4', '7', '0', '0', '0', '18', '1', '3', '0', '略');
-INSERT INTO `questions` VALUES ('11', 'fghhgjdgh', '6', '5', '0', '0', '0', '17', '3', '2', '0', '略');
-INSERT INTO `questions` VALUES ('12', 'vbvbb', '1', '3', '0', '0', '0', '16', '4', '1', '0', '略');
-INSERT INTO `questions` VALUES ('13', '唐诗', '1', '5', '0', '0', '0', '2', '2', '1', '0', '略');
-INSERT INTO `questions` VALUES ('14', '修改病句', '1', '3', '4', '0', '0', '6', '3', '2', '0', '略');
-INSERT INTO `questions` VALUES ('15', '幂函数', '2', '14', '19', '0', '0', '2', '2', '2', '0', '略');
-INSERT INTO `questions` VALUES ('16', '测试题目', '1', '1', '3', '4', '0', '1', '5', '3', '0', '略');
-INSERT INTO `questions` VALUES ('17', '测试2', '6', '11', '16', '0', '0', '6', '3', '1', '0', '略');
-INSERT INTO `questions` VALUES ('18', '测试题目内容2', '6', '11', '16', '0', '0', '6', '3', '1', '0', '略');
-INSERT INTO `questions` VALUES ('19', '11', '2', '14', '19', '0', '0', '1', '5', '3', '0', '略');
-INSERT INTO `questions` VALUES ('20', 'tsest111', '1', '1', '3', '4', '0', '1', '5', '3', '0', '略');
-INSERT INTO `questions` VALUES ('21', '蛋白质', '6', '21', '22', '0', '0', '1', '4', '3', '0', '略');
-INSERT INTO `questions` VALUES ('22', '糖类de zhonglei', '6', '21', '23', '0', '0', '19', '1', '2', '0', '略');
-INSERT INTO `questions` VALUES ('23', '货币de jiazhi', '8', '24', '25', '0', '0', '19', '3', '2', '0', '略');
-INSERT INTO `questions` VALUES ('24', '消费zenmesuan', '8', '24', '25', '0', '0', '19', '5', '1', '0', '略');
-INSERT INTO `questions` VALUES ('25', '极限的定义', '2', '14', '26', '0', '0', '20', '3', '3', '0', '略');
-INSERT INTO `questions` VALUES ('26', 'hanshujiexishi', '2', '14', '19', '0', '0', '19', '4', '2', '0', '略');
-INSERT INTO `questions` VALUES ('27', '反函数的性质', '2', '14', '19', '29', '0', '9', '2', '1', '0', '略');
+INSERT INTO `questions` VALUES ('1', 'qewretyytiyopi', 'localhost:8111/AutoGenPaperSystem/upload/question/question_id1.JPG', '1', '1', '3', '4', '0', '1', '5', '3', '0', '略', 'localhost:8111/AutoGenPaperSystem/upload/answer/answer_id1.JPG');
+INSERT INTO `questions` VALUES ('2', 'uijsidofoei', 'localhost:8111/AutoGenPaperSystem/upload/question/question_id2.JPG', '3', '2', '0', '0', '0', '3', '4', '1', '0', '略', null);
+INSERT INTO `questions` VALUES ('3', 'dajfioef', 'localhost:8111/AutoGenPaperSystem/upload/question/question_id3.JPG', '4', '6', '0', '0', '0', '8', '3', '3', '0', '略', null);
+INSERT INTO `questions` VALUES ('4', 'ffbfb', null, '5', '2', '0', '0', '0', '7', '2', '2', '0', '略', null);
+INSERT INTO `questions` VALUES ('8', 'dfefdfef', null, '6', '11', '16', '0', '0', '6', '3', '1', '0', '略', null);
+INSERT INTO `questions` VALUES ('9', 'ghdjghg', null, '3', '9', '0', '0', '0', '13', '3', '2', '0', '略', null);
+INSERT INTO `questions` VALUES ('10', 'trhtjfgt', null, '4', '7', '0', '0', '0', '18', '1', '3', '0', '略', null);
+INSERT INTO `questions` VALUES ('11', 'fghhgjdgh', null, '6', '5', '0', '0', '0', '17', '3', '2', '0', '略', null);
+INSERT INTO `questions` VALUES ('13', '唐诗', 'localhost:8111/AutoGenPaperSystem/upload/question/question_id13.JPG', '1', '5', '0', '0', '0', '2', '2', '1', '0', '略', 'localhost:8111/AutoGenPaperSystem/upload/answer/answer_id13.JPG');
+INSERT INTO `questions` VALUES ('14', '修改病句', 'localhost:8111/AutoGenPaperSystem/upload/question/question_id14.JPG', '1', '1', '3', '4', '0', '6', '3', '2', '0', '略', 'localhost:8111/AutoGenPaperSystem/upload/answer/answer_id14.JPG');
+INSERT INTO `questions` VALUES ('15', '幂函数', null, '2', '14', '19', '0', '0', '2', '2', '2', '0', '略', null);
+INSERT INTO `questions` VALUES ('16', '测试题目', 'localhost:8111/AutoGenPaperSystem/upload/question/question_id16.JPG', '1', '1', '3', '4', '0', '1', '5', '3', '0', '略', 'localhost:8111/AutoGenPaperSystem/upload/answer/answer_id16.JPG');
+INSERT INTO `questions` VALUES ('17', '测试2', null, '6', '11', '16', '0', '0', '6', '3', '1', '0', '略', null);
+INSERT INTO `questions` VALUES ('18', '测试题目内容2', null, '6', '11', '16', '0', '0', '6', '3', '1', '0', '略', null);
+INSERT INTO `questions` VALUES ('19', '11', null, '2', '14', '19', '0', '0', '1', '5', '3', '0', '略', null);
+INSERT INTO `questions` VALUES ('20', 'tsest111', 'localhost:8111/AutoGenPaperSystem/upload/question/question_id20.JPG', '1', '1', '3', '4', '0', '1', '5', '3', '0', '略', 'localhost:8111/AutoGenPaperSystem/upload/answer/answer_id20.JPG');
+INSERT INTO `questions` VALUES ('21', '蛋白质', null, '6', '21', '22', '0', '0', '1', '4', '3', '0', '略', null);
+INSERT INTO `questions` VALUES ('23', '货币de jiazhi', null, '8', '24', '25', '0', '0', '19', '3', '2', '0', '略', null);
+INSERT INTO `questions` VALUES ('24', '消费zenmesuan', null, '8', '24', '25', '0', '0', '19', '5', '1', '0', '略', null);
+INSERT INTO `questions` VALUES ('25', '极限的定义', null, '2', '14', '26', '0', '0', '20', '3', '3', '0', '略', null);
+INSERT INTO `questions` VALUES ('26', 'hanshujiexishi', null, '2', '14', '19', '0', '0', '20', '4', '2', '0', '略', null);
+INSERT INTO `questions` VALUES ('27', '反函数的性质', null, '2', '14', '19', '29', '0', '9', '2', '1', '0', '略', null);
+INSERT INTO `questions` VALUES ('28', '诗', 'localhost:8111/AutoGenPaperSystem/upload/question/question_id28.JPG', '1', '5', '0', '0', '0', '5', '3', '1', '0', '略', 'localhost:8111/AutoGenPaperSystem/upload/answer/answer_id28.JPG');
+INSERT INTO `questions` VALUES ('29', '如何断句', 'localhost:8111/AutoGenPaperSystem/upload/question/question_id29.JPG', '1', '6', '17', '0', '0', '3', '2', '2', '0', '略', 'localhost:8111/AutoGenPaperSystem/upload/answer/answer_id29.JPG');
+INSERT INTO `questions` VALUES ('30', '指南针的发明', null, '9', '34', '35', '0', '0', '3', '5', '3', '0', '略', null);
+INSERT INTO `questions` VALUES ('31', '蛋白质的产生', null, '6', '32', '33', '0', '0', '7', '1', '2', '0', '略', null);
+INSERT INTO `questions` VALUES ('32', '电解液原理', null, '5', '38', '39', '0', '0', '6', '4', '2', '0', '略', null);
+INSERT INTO `questions` VALUES ('33', '化学毒性', null, '5', '40', '41', '0', '0', '0', '2', '2', '0', '略', null);
+INSERT INTO `questions` VALUES ('34', '大型蛋白质', null, '6', '32', '33', '0', '0', '4', '2', '1', '0', '略', null);
+INSERT INTO `questions` VALUES ('35', '糖类的水解', null, '6', '32', '33', '0', '0', '12', '3', '3', '0', '略', null);
+INSERT INTO `questions` VALUES ('36', '化学安全实验', null, '5', '38', '39', '0', '0', '6', '4', '2', '0', '略', null);
+INSERT INTO `questions` VALUES ('37', '世界地图', null, '9', '36', '37', '0', '0', '8', '1', '3', '0', '略', null);
+INSERT INTO `questions` VALUES ('38', '子曰', 'localhost:8111/AutoGenPaperSystem/upload/question/question_id38.JPG', '1', '1', '0', '0', '0', '1', '1', '3', '0', '略', 'localhost:8111/AutoGenPaperSystem/upload/answer/answer_id38.JPG');
+INSERT INTO `questions` VALUES ('39', '孔子', 'localhost:8111/AutoGenPaperSystem/upload/question/question_id39.JPG', '1', '1', '0', '0', '0', '1', '1', '3', '0', '略', 'localhost:8111/AutoGenPaperSystem/upload/answer/answer_id39.JPG');
+INSERT INTO `questions` VALUES ('40', '孟子', 'localhost:8111/AutoGenPaperSystem/upload/question/question_id40.JPG', '1', '1', '0', '0', '0', '1', '1', '3', '0', '略', 'localhost:8111/AutoGenPaperSystem/upload/answer/answer_id40.JPG');
+INSERT INTO `questions` VALUES ('41', '学而不思', 'localhost:8111/AutoGenPaperSystem/upload/question/question_id41.JPG', '1', '1', '0', '0', '0', '1', '1', '3', '0', '略', 'localhost:8111/AutoGenPaperSystem/upload/answer/answer_id41.JPG');
+INSERT INTO `questions` VALUES ('42', '测试题目', 'localhost:8111/AutoGenPaperSystem/upload/question/question_id42.JPG', '1', '1', '0', '0', '0', '1', '1', '3', '0', '略', 'localhost:8111/AutoGenPaperSystem/upload/answer/answer_id42.JPG');
+INSERT INTO `questions` VALUES ('43', '请点击下面链接激活您的帐号（如无法点击，请复制网址到浏览器地址栏敲回车即可）： \r\n', 'localhost:8111/AutoGenPaperSystem/upload/question/question_id43.JPG', '1', '1', '0', '0', '0', '1', '1', '3', '0', '略', 'localhost:8111/AutoGenPaperSystem/upload/answer/answer_id43.JPG');
+INSERT INTO `questions` VALUES ('44', '题目测试', 'localhost:8111/AutoGenPaperSystem/upload/question/question_id44.JPG', '1', '1', '0', '0', '0', '1', '1', '3', '0', '略', 'localhost:8111/AutoGenPaperSystem/upload/answer/answer_id44.JPG');
+INSERT INTO `questions` VALUES ('45', '1222323为323<dqljbe61503@chacuo.net>', 'localhost:8111/AutoGenPaperSystem/upload/question/question_id45.JPG', '1', '1', '0', '0', '0', '1', '1', '3', '0', '略', 'localhost:8111/AutoGenPaperSystem/upload/answer/answer_id45.JPG');
+INSERT INTO `questions` VALUES ('46', 'http://24mail.chacuo.net/', 'localhost:8111/AutoGenPaperSystem/upload/question/question_id46.JPG', '1', '1', '0', '0', '0', '1', '1', '3', '0', '略', 'localhost:8111/AutoGenPaperSystem/upload/answer/answer_id46.JPG');
+INSERT INTO `questions` VALUES ('47', '/1/1/1/1/1/1//1/1/1/1/1//1/1/1/1//1/sbshbs.jpg', 'localhost:8111/AutoGenPaperSystem/upload/question/question_id47.JPG', '1', '1', '0', '0', '0', '1', '1', '3', '0', '略', 'localhost:8111/AutoGenPaperSystem/upload/answer/answer_id47.JPG');
+INSERT INTO `questions` VALUES ('48', '/1/1/1/1//1//2/2/2/2/3//3/3/3/34/44/', 'localhost:8111/AutoGenPaperSystem/upload/question/question_id48.JPG', '1', '1', '0', '0', '0', '1', '1', '3', '0', '略', 'localhost:8111/AutoGenPaperSystem/upload/answer/answer_id48.JPG');
+INSERT INTO `questions` VALUES ('49', 'wwww.baidu.com', 'localhost:8111/AutoGenPaperSystem/upload/question/question_id49.JPG', '1', '1', '0', '0', '0', '1', '1', '3', '0', '略', 'localhost:8111/AutoGenPaperSystem/upload/answer/answer_id49.JPG');
 
 -- ----------------------------
 -- Table structure for subject
@@ -169,7 +203,7 @@ CREATE TABLE `subject` (
   `Subject_name` varchar(20) COLLATE utf8_bin NOT NULL,
   `Grade_ID` int(11) NOT NULL,
   PRIMARY KEY (`Subject_ID`)
-) ENGINE=InnoDB AUTO_INCREMENT=9 DEFAULT CHARSET=utf8 COLLATE=utf8_bin;
+) ENGINE=InnoDB AUTO_INCREMENT=10 DEFAULT CHARSET=utf8 COLLATE=utf8_bin;
 
 -- ----------------------------
 -- Records of subject
@@ -182,6 +216,7 @@ INSERT INTO `subject` VALUES ('5', '化学', '3');
 INSERT INTO `subject` VALUES ('6', '生物', '3');
 INSERT INTO `subject` VALUES ('7', '科学', '2');
 INSERT INTO `subject` VALUES ('8', '政治', '1');
+INSERT INTO `subject` VALUES ('9', '历史', '3');
 
 -- ----------------------------
 -- Table structure for types
