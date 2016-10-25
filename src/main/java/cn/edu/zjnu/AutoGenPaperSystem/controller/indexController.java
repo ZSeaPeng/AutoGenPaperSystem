6de11051e6b04214e157cad524902f3ff3e13797
zseapeng <zseapeng@163.com>
2016-10-25 14:32:31 +0800

    增加更新数据测试

---
+++ src/main/java/cn/edu/zjnu/AutoGenPaperSystem/controller/indexController.java
@@ -0,0 +1,30 @@
+package cn.edu.zjnu.AutoGenPaperSystem.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.ResponseBody;
+
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
+/**
+ * Created by zseapeng on 2016/10/24.
+ */
+@Controller
+@RequestMapping(value = "/api/info")
+@ResponseBody
+public class indexController {
+    @RequestMapping(method = RequestMethod.GET)
+    public Map getUpdateInfo() {
+        Map<String,List> map = new HashMap<String, List>();
+        List<String> subList = new ArrayList<String>();
+        subList.add("语文");
+        subList.add("数学");
+        subList.add("机械类");
+        map.put("科目",subList);
+        return map;
+    }
+}
