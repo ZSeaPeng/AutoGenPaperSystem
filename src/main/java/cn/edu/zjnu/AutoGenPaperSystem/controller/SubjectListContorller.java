e5dc158ff4afe9e205347206a363bdf7f0991ac3
zseapeng <zseapeng@163.com>
2016-10-28 15:39:08 +0800

    处理updateinfo json 数据

--- src/main/java/cn/edu/zjnu/AutoGenPaperSystem/controller/SubjectListContorller.java
+++ src/main/java/cn/edu/zjnu/AutoGenPaperSystem/controller/SubjectListContorller.java
@@ -1,6 +1,7 @@
 package cn.edu.zjnu.AutoGenPaperSystem.controller;
 
-import cn.edu.zjnu.AutoGenPaperSystem.model.Questions;
+import cn.edu.zjnu.AutoGenPaperSystem.model.UpdateInfoJson;
+import cn.edu.zjnu.AutoGenPaperSystem.service.QuestionsService;
 import cn.edu.zjnu.AutoGenPaperSystem.service.SubjectService;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -8,10 +9,7 @@ import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.ResponseBody;
 
 import javax.annotation.Resource;
-import java.util.ArrayList;
-import java.util.HashMap;
-import java.util.List;
-import java.util.Map;
+import java.util.*;
 
 /**
  * Created by zseapeng on 2016/9/26.
@@ -23,32 +21,62 @@ public class SubjectListContorller {
 
     @Resource
     private SubjectService subjectServiceImpl;
+    @Resource
+    private QuestionsService questionsServiceImpl;
 
     @RequestMapping(method = RequestMethod.GET)
     public List getSubjectList() {
-        List subList =  subjectServiceImpl.selectAllSubject();
-        Map<String,List> map = new HashMap<String, List>();
-        List<String> updatesubList = new ArrayList<String>();
+        List subList = subjectServiceImpl.selectAllSubject();
+        Map<String, List> map = new HashMap<String, List>();
+        List<List> updatesubList = new ArrayList<List>();
+        updatesubList.clear();
+        List<UpdateInfoJson> firstUpdateInfo = questionsServiceImpl.selectUploadTime();
+        List<String> dateTemp = new ArrayList<String>();
+        List one = new ArrayList();
+        one.clear();
+        List two = new ArrayList();
+        two.clear();
+        List three = new ArrayList();
+        three.clear();
+        for (UpdateInfoJson u : firstUpdateInfo) {
+            dateTemp.add(u.getDate().substring(0, 10));
+        }
+        dateTemp = removeDuplicate(dateTemp);
 
-        List<Questions> firstUpdateInfo = new ArrayList();
-        List secondUpdateInfo = new ArrayList();
-        List thirdUpdateInfo = new ArrayList();
+        for (UpdateInfoJson u : firstUpdateInfo) {
+            if (u.getDate().substring(0, 10).equalsIgnoreCase(dateTemp.get(0))) {
+                one.add(u);
+            } else if (u.getDate().substring(0, 10).equalsIgnoreCase(dateTemp.get(1))) {
+                two.add(u);
+            } else if (u.getDate().substring(0, 10).equalsIgnoreCase(dateTemp.get(2))) {
+                three.add(u);
+            }
+        }
 
-        //for (Questions questions:firstUpdateInfo){
-        //    UpdateInfo updateInfo = new UpdateInfo();
-        //    updateInfo.setSub(questions.get);
-        //}
 
-
-        updatesubList.add("语文");
-        updatesubList.add("数学");
-        updatesubList.add("机械类");
-        map.put("sublist",subList);
-        map.put("update",updatesubList);
+        updatesubList.add(one);
+        updatesubList.add(two);
+        updatesubList.add(three);
+        map.put("sublist", subList);
+        map.put("update", updatesubList);
         List allList = new ArrayList();
         //allList.add(subList);
         allList.add(map);
         return allList;
+    }
+
+
+    private List removeDuplicate(List list) {
+        Set set = new HashSet();
+        List newList = new ArrayList();
+        for (Iterator iter = list.iterator(); iter.hasNext(); ) {
+            Object element = iter.next();
+            if (set.add(element))
+                newList.add(element);
+        }
+        list.clear();
+        list.addAll(newList);
+        return list;
     }
 
 }
