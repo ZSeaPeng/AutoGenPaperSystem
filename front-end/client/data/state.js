9e6c1c885f72a57579a069b00c7f3ae7dc21db16
GTyexing <w1404278166@gmail.com>
2016-11-02 12:03:28 +0800

    数据格式，尽量按这个格式发个我

---
+++ front-end/client/data/state.js
@@ -0,0 +1,32 @@
+state = {
+  grades: {
+    sublist:[
+      {
+        contextList:[
+          {context: "", url: ""},
+          {},
+          ...{}
+        ]
+      }
+    ],
+    update: [
+      [{sub: "", url: "", date: ""},{},...{}],
+      [{},{},...{}],
+      [{},{},...{}]
+    ],
+    img: ["", "", ""]
+  },
+  select: {
+    Types: [{name: "", url: "", select: false},{},...{}],
+    Points: [{level: 0, name: "", url: "", pointList: [], select: false},{},...{}],
+    Charaction: [{name: "", url: "", select: false},{},...{}],
+    Difficulty: [{name: "", url: "", select: false},{},...{}]
+  },
+  question: {
+    context: [{id: 1, qurl: "", aurl: "", expanded: false},{},...{}],-
+    pageNum: 1,
+    pages: 1,
+    userChosen: [{id: 1, type: ""},{},...{}],
+    userCollection: [...id]
+  }
+}
