0264552f96457e4619f611574af4416bef2fec69
Eddie <1289030653@qq.com>
2016-12-07 20:09:50 +0800

    自动组卷页面

--- front-end/client/container/ManualCombine.js
+++ front-end/client/container/ManualCombine.js
@@ -1,32 +1,98 @@
 import React, { Component } from 'react';
+import { connect } from 'react-redux';
 
-import ConfirmButton from '../components/ConfirmButton';
 import ChooseCourse from '../components/ChooseCourse';
 import ChooseModel from '../components/ChooseModel';
+import RaisedButton from 'material-ui/RaisedButton';
 
+import { submitModel } from '../actions/actionCreators';
 const styles = {
+  confirmButton: {
+    width: "16%",
+    height: 50,
+    marginLeft: "42%",
+    marginTop: 30,
+  },
   title: {
     fontFamily: "Microsoft YaHei",
     fontWright: "normal",
     color: "#333"
   },
+  parent: {
+    position: 'absolute',
+    left: '7%',
+    width: '1100px'
+  },
+  children: {
+    margin: 'auto'
+  }
 };
 
 class ManualCombine extends Component {
 
+  // state = {
+  //   value: 0,
+  //   model: "model1",
+  // };
+  constructor (props) {
+    super(props);
+    this.handleSubmit = this.handleSubmit.bind(this);
+    this.state = { value: 0, model: 1 };
+  }
+  onCourseChanged = (newValue) => {
+    this.setState({value: newValue});
+  };
+  onModelChanged = (newModel) => {
+    this.setState({model: newModel});
+  };
+  handleSubmit() {
+    const { dispatch } = this.props;
+    const wordtype = this.state.model;
+    const subject = this.props.sublist[0].contextList[this.state.value].subid;
+    dispatch(submitModel(wordtype, subject));
+  };
+
 	render() {
+    const { sublist } = this.props;
+    const isEmpty = sublist.length === 0;
 		return (
-          <div style={{margin: 'auto', width: '75%'}}>
-			<h2 style={styles.title}>手动组卷</h2><br/>
-			<hr/>
-			<ChooseCourse/>
+      <div style={styles.parent}>
+  			<h2 style={styles.title}>手动组卷</h2><br/>
+  			<hr/>
+        <div style={styles.children}>
+          { isEmpty? console.log("empty") :
+            <ChooseCourse courses={ sublist[0].contextList } callback={this.onCourseChanged}/>
+          }
+        </div>
 		    <br/><hr/>
-		    <ChooseModel/>
+        <div style={{height:300,margin:'auto'}}>
+		      <ChooseModel callback={this.onModelChanged}/>
+        </div>
 		    <br/>
-		    <ConfirmButton/>
+        <div>
+          { isEmpty? <div></div> :
+            // <RaisedButton
+            //   label="选择试题"
+            //   secondary={true}
+            //   href={sublist[0].contextList[this.state.value].url}
+            //   style={styles.confirmButton}
+            // />
+            <button style={styles.confirmButton} onClick={this.handleSubmit}>选择试题</button>
+          }
+          {console.log(this.state.model)}
+        </div>
+        <p style={{padding:20}}></p>
 		  </div>
 		)
 	}
 };
 
-export default ManualCombine;
+const mapStateToProps = state => {
+  const { grades } = state;
+  const { sublist } = grades;
+  return {
+    sublist
+  }
+}
+
+export default connect(mapStateToProps)(ManualCombine);
