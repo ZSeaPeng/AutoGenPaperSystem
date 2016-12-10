c73e9efb2b6c1613f85a57249365ddbc4570f073
GTyexing <w1404278166@gmail.com>
2016-12-10 15:05:49 +0800

    ...

--- front-end/client/components/News.js
+++ front-end/client/components/News.js
@@ -6,7 +6,11 @@ import {Card, CardText} from 'material-ui/Card';
 
 class News extends Component {
   render() {
-    const { news } = this.props;
+    let { news } = this.props;
+    for (let i = 0; i < news.length; i++) {
+      news[i] === [];
+      news = [ ...news.slice(0, i), ...news.slice(i + 1) ]
+    }
     const style = {
       Card: {
         backgroundColor: '#FAFAFA',
@@ -25,9 +29,11 @@ class News extends Component {
           <Card style = {style.Card} key={i}>
             <CardText>
               {nee.map((neee, i) =>
-                <Link to={neee.url} key={i}>{neee.sub} </Link>)} 有更新
-                <br/>
-                <small style={{color: '#B3B3B3'}}>{ nee[0].date }</small>
+                <Link to={neee.url} key={i}>{neee.sub} </Link>)
+              }
+              有更新
+              <br/>
+              <small style={{color: '#B3B3B3'}}>{ nee[0].date }</small>
             </CardText>
           </Card>
         )}
