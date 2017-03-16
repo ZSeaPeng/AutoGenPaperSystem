388af4d9a9e76c71352c82c04b0ea6fef21b75ed
吴晗 <wuhan@wuhandeMacBook-Pro.local>
2017-03-16 21:12:52 +0800

    自动组卷

--- front-end/client/actions/actionCreators.js
+++ front-end/client/actions/actionCreators.js
@@ -831,14 +831,7 @@ export const submitWordInfo = (wordInfo) => dispatch => {
       ...wordInfo
     })
   })
-<<<<<<< HEAD
     .then(response => response.json())
     .then(dispatch(testPaper(json)))
     .then(browserHistory.push("/autotestpaper"))
-
-=======
-  .then(response => response.json())
-  .then(json =>
-      browserHistory.push(json.url))
->>>>>>> origin/master
 };
