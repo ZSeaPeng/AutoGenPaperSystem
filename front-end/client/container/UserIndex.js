34130002534297f35a64cdab30c70324181937a2
sgt110 <1067541968@qq.com>
2017-03-27 17:41:19 +0800

    小错误

--- front-end/client/container/UserIndex.js
+++ front-end/client/container/UserIndex.js
@@ -94,7 +94,7 @@ class UserIndex extends React.Component {
     const { dispatch } = this.props;
     const phone = this.state.phone;
     const email = this.state.email;
-    dispatch(asynChangeInfo(email， phone))
+    dispatch(asynChangeInfo(email, phone))
     this.setState({open: false, open1: false, open2: false, phone: '', email: ''})
   }
 
