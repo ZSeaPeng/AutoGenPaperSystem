1fb5e7f53654540cb41b5537cee0e81dfcedec9b
zseapeng <zseapeng@163.com>
2017-04-23 19:39:00 +0800

    增加随机换题方法

--- src/main/java/cn/edu/zjnu/AutoGenPaperSystem/controller/PaperController.java
+++ src/main/java/cn/edu/zjnu/AutoGenPaperSystem/controller/PaperController.java
@@ -52,6 +52,7 @@ public class PaperController {
         return infoMap;
     }
 
+
     @RequestMapping(value = "/makepaper", method = RequestMethod.POST)
     public Map getTest(@RequestBody Map map, @ModelAttribute("userid") Integer userid,
                        HttpServletRequest request) {
@@ -74,7 +75,7 @@ public class PaperController {
         //清除数据库的存储试题
         userServiceImpl.deleteUserChonce(userid);
         //开始组卷
-        String questionids="";
+        String questionids = "";
         String randomStr = RandomStr.getRandomString(6);
         Calendar now = Calendar.getInstance();
         String date = "" + now.get(Calendar.YEAR) + (now.get(Calendar.MONTH) + 1) + now.get(Calendar.DAY_OF_MONTH);
@@ -91,23 +92,23 @@ public class PaperController {
             List answerList = new ArrayList();
             List<Integer> idList = (List<Integer>) list.get(i).get("id");
             for (Integer id : idList) {
-                questionids=questionids+id+",";
+                questionids = questionids + id + ",";
                 Questions questions = questionsServiceImpl.selectByPrimaryKey(id);
-                questionList.add("http://papersystem01.oss-cn-hangzhou.aliyuncs.com/"+questions.getContent());
-                answerList.add("http://papersystem01.oss-cn-hangzhou.aliyuncs.com/"+questions.getAnswer());
+                questionList.add("http://papersystem01.oss-cn-hangzhou.aliyuncs.com/" + questions.getContent());
+                answerList.add("http://papersystem01.oss-cn-hangzhou.aliyuncs.com/" + questions.getAnswer());
             }
 
             questionMap.put(list.get(i).get("type") + "(共" + questionList.size() + "题，共" + list.get(i).get("score") + "分)", questionList);
             answerMap.put(list.get(i).get("type") + "(共" + questionList.size() + "题，共" + list.get(i).get("score") + "分)", answerList);
         }
         try {
-            String fileName=date + "u" + userid + PinyinHelper.convertToPinyinString(subName, "", PinyinFormat.WITHOUT_TONE) + randomStr;
+            String fileName = date + "u" + userid + PinyinHelper.convertToPinyinString(subName, "", PinyinFormat.WITHOUT_TONE) + randomStr;
             String qurl = request.getSession().getServletContext().getRealPath("/upload/temp/" + fileName + ".docx");
             setAllDocx.Title(questionMap, request.getSession().getServletContext().getRealPath("/upload/template/templateA3Horizontal.docx"), qurl);
             String aurl = request.getSession().getServletContext().getRealPath("/upload/temp/a_" + fileName + ".docx");
             setAllDocx.Title(answerMap, request.getSession().getServletContext().getRealPath("/upload/template/templateA3Horizontal.docx"), aurl);
             //纪录组成的试卷
-            Paper paper=new Paper();
+            Paper paper = new Paper();
             paper.setPapername(subName + title);
             paper.setUserid(userid);
             paper.setAnswerurl(aurl);
@@ -129,89 +130,12 @@ public class PaperController {
         return paperMap;
     }
 
+    @RequestMapping(value = "/randomchange", method = RequestMethod.GET)
+    public void randomchange() {
 
-    //public Map getPaperList(@RequestBody PaperObject[] paperObjects, @ModelAttribute("userid") Integer userid,
-    //                        HttpServletRequest request) {
-    //    System.out.println("--------");
-    //    String fileName = "测试文件";
-    //    User user = userServiceImpl.selectByPrimaryKey(userid);
-    //    user.setUserchosen("0");
-    //    user.setAdd(new ArrayList());
-    //    user.setUserpassword(null);
-    //    userServiceImpl.updateByPrimaryKeySelective(user);
-    //    String chosenList = "0";
-    //    System.out.println("size--->"+paperObjects.length);
-    //    for (PaperObject s:paperObjects){
-    //        System.out.println("s---->"+s.toString());
-    //    }
-    //    return null;
-    //
-    //}
+    }
 
-    //@RequestMapping(value = "/makepaper", method = RequestMethod.POST)
-    //public Map getPaperList(@RequestBody Integer[] paperlist, @ModelAttribute("userid") Integer userid,
-    //                           HttpServletRequest request) {
-    //    String fileName = "测试文档";
-    //    User user = userServiceImpl.selectByPrimaryKey(userid);
-    //    user.setUserchosen("0");
-    //    user.setAdd(new ArrayList());
-    //    user.setUserpassword(null);
-    //    userServiceImpl.updateByPrimaryKeySelective(user);
-    //    String chosenList = "0";
-    //    for (int p : paperlist) {
-    //        chosenList = chosenList + "," + String.valueOf(p);
-    //    }
-    //
-    //    Paper paper = new Paper();
-    //    paper.setUserId(userid);
-    //    paper.setQuestionIds(chosenList);
-    //
-    //    paperServiceImpl.insertSelective(paper);
-    //    Map<String,Object> qurMap=new HashMap<>();
-    //    Map<String,Object> aurMap=new HashMap<>();
-    //    List QurlList = new ArrayList();
-    //    List AurlList = new ArrayList();
-    //    for (Integer i : paperlist) {
-    //        Questions questions = questionsServiceImpl.selectByPrimaryKey(i);
-    //        QurlList.add(questions.getContent());
-    //        AurlList.add(questions.getAnswer());
-    //    }
-    //    qurMap.put(" ",QurlList);
-    //    aurMap.put(" ",AurlList);
-    //    try {
-    //        String qurl = request.getServletContext().getRealPath("/upload/temp/" + PinyinHelper.convertToPinyinString(fileName, "", PinyinFormat.WITHOUT_TONE) + ".docx");
-    //        SetAllDocx.Title(qurMap,request.getServletContext().getRealPath("/upload/template/templateA4Vertical.docx"),qurl);
-    //        String aurl = request.getServletContext().getRealPath("/upload/temp/a_" + PinyinHelper.convertToPinyinString(fileName, "", PinyinFormat.WITHOUT_TONE) + ".docx");
-    //        SetAllDocx.Title(aurMap,request.getServletContext().getRealPath("/upload/template/templateA4Vertical.docx"),aurl);
-    //        System.out.println("-------------");
-    //        Map map = new HashMap();
-    //        map.put("qurl","localhost:8111/AutoGenPaperSystem/upload/temp/"+ PinyinHelper.convertToPinyinString(fileName, "", PinyinFormat.WITHOUT_TONE)+".docx");
-    //        map.put("aurl","localhost:8111/AutoGenPaperSystem/upload/temp/a_"+ PinyinHelper.convertToPinyinString(fileName, "", PinyinFormat.WITHOUT_TONE)+".docx");
-    //        return map;
-    //
-    //        //return
-    //    } catch (Exception e) {
-    //        e.printStackTrace();
-    //    }
-    //    //String dfileName = new String(fileName.getBytes("gb2312"), "iso8859-1");
-    //    //HttpHeaders headers = new HttpHeaders();
-    //    //headers.setContentType(MediaType.APPLICATION_OCTET_STREAM);
-    //    //headers.setContentDispositionFormData("attachment", dfileName);
-    //    //return new ResponseEntity<byte[]>(FileUtils.readFileToByteArray(file), headers, HttpStatus.CREATED);
-    //    return null;
-    //
-    //}
 
-    //@RequestMapping(value = "/auto", method = RequestMethod.POST)
-    //public void getAutoInfo(@RequestBody Map map) {
-    //    System.out.println("--");
-    //    System.out.println(map.get("wordtype").toString());
-    //
-    //    System.out.println(map.get("subject"));
-    //    System.out.println(map.get("diff"));
-    //    System.out.println(map.get("questions"));
-    //    //System.out.println(map.get("questions"));
-    //    System.out.println(map.get("points"));
-    //}
+
 }
 
