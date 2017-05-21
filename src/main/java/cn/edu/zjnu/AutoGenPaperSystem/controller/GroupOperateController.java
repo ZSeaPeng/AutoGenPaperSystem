a30d49a81792c8e6942270c04fd8708abab72008
sgt110 <1067541968@qq.com>
2017-05-21 11:21:32 +0800

    集团用户修改密码

--- src/main/java/cn/edu/zjnu/AutoGenPaperSystem/controller/GroupOperateController.java
+++ src/main/java/cn/edu/zjnu/AutoGenPaperSystem/controller/GroupOperateController.java
@@ -1,13 +1,17 @@
 package cn.edu.zjnu.AutoGenPaperSystem.controller;
 
+import cn.edu.zjnu.AutoGenPaperSystem.model.ComManager;
 import cn.edu.zjnu.AutoGenPaperSystem.model.ComManagerJson;
 import cn.edu.zjnu.AutoGenPaperSystem.model.User;
 import cn.edu.zjnu.AutoGenPaperSystem.service.ComManagerService;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.*;
+import org.springframework.web.bind.support.SessionStatus;
 
 import javax.annotation.Resource;
+import java.util.ArrayList;
 import java.util.List;
+import java.util.Map;
 
 /**
  * Created by zseapeng on 2016/11/28.
@@ -33,5 +37,16 @@ public class GroupOperateController {
         return null;
     }
 
+    @RequestMapping(value = "/change", method = RequestMethod.POST)
+    public String changePassword(@RequestBody Map password, @ModelAttribute("userid") Integer userid, SessionStatus sessionStatus) {
+        ComManager comManager = comManagerServiceImpl.selectByPrimaryKey(userid);
+        comManager.setCommanagerPsw(String.valueOf(password.get("password")));
+//        comManager.setAdd(new ArrayList());
+        if (comManagerServiceImpl.updateByPrimaryKeySelective(comManager) != 0) {
+            sessionStatus.setComplete();
+            return "{\"success\":\"true\"}";
+        }
+        return "{\"success\":\"false\"}";
+    }
 
 }
